9d1ffc02148ff8aa8cd4c558025400d64c15be61
L <desheng.yuan@outlook.com>
2023-08-14 22:39:37 +0800

    Day2-01-轮播图-通用组件自动导入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 //
 import CustomNavbar from '@/pages/index/componets/CustomNavbar.vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 </script>
 
 <template>
   <custom-navbar></custom-navbar>
+
+  <xtx-swiper></xtx-swiper>
+
   <view class="index">index</view>
-  <uni-card title="基础卡片" extra="额外信息">
-    <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss">
