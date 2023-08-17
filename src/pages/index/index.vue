5e76b065ae454f05fab9257057743c571d6d6d0c
L <desheng.yuan@outlook.com>
2023-08-17 22:35:50 +0800

    Day2-06-前台分类-组件封装 Day2-07-前台分类-获取数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,29 +1,47 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/componets/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from '@/pages/index/componets/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
+
+// 获取轮播图数据
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
 
+// 获取前台分类数据
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  console.log(res)
+}
+
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
 
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+
+  <!-- 分类面板 -->
+  <CategoryPanel />
 
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
 //
+page {
+  background-color: #f7f7f7;
+}
 </style>
